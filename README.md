6b6b076ccd974e75accf74645a229a2d3dc4ab9d
w626023781@163.com <w626023781@163.com>
2021-11-04 22:21:45 +0800

    整理依赖，侧重文档

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # NKXRB的博客（NKXRB-BLOG）
 
-基于vue + vite 的架构开发，已学习技术、记录笔记、测试DEMO为主
+基于vuepress + vite 的架构开发，已学习技术、记录笔记、测试DEMO为主
+[在线预览](https://nkxrb.github.io/docs/)
 
 ## 使用到的技术
+- vuepress
 - vue3.0
 - vite
 - typescript
-- scss
 
 ## 涉及到的内容
 - css进阶知识
+- 高阶JS函数技巧
 - leetcode练习册
-- 
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,19 +5,6 @@ import Example from '@/components/layout/Example.vue'
 import LeanCss from '@/views/leancss/index.vue'
 import LeetCode from '@/views/leetcode/index.vue'
 import Developing from '@/views/error/developing.vue'
-import examples from '@/config/examples.json'
-
-const layzLoad = (viewname: string) => () => import(/* @vite-ignore */'../examples/' + viewname)
-
-const exampleRoutes = examples.map(item => {
-  let routeView: RouteRecordRaw = {
-    path: '/examples/' + item.name,
-    name: item.name,
-    component: layzLoad(item.name)
-  }
-  return routeView
-})
-
 
 const routes: RouteRecordRaw[] = [
   {
@@ -33,8 +20,7 @@ const routes: RouteRecordRaw[] = [
   {
     name: 'example',
     path: '/example',
-    component: Example,
-    children: exampleRoutes
+    component: Example
   },
   {
     path: '/leancss/:label',
